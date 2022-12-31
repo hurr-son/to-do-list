72a9901966441afb502530ae1be19351daf027c6
hurr-son <hprewett05@gmail.com>
2022-12-31 12:53:22 -0600

    created a Projects class.
    I wanted a way to store projects in an array
    and manipulate that array.
    
    This was also needed in order to render
    all of the projects on the page

--- src/project.js
+++ src/project.js
@@ -1,24 +1,50 @@
-function Project(name) {
-    this.name = name;
-    this.todoLists = [];
+class Projects {
+    constructor() {
+      this.projects = [];
+  
+      this.Project = function(name) {
+        this.name = name;
+        this.todoLists = [];
+  
+        this.addList = function(list) {
+            this.todoLists.push(list);
+        };
+  
+        this.removeList = function(list) {
+            let index = this.todoLists.indexOf(list);
+            if (index > -1) {
+                this.todoLists.splice(index, 1);
+            }
+        };
+  
+        this.displayLists = function() {
+            console.log("Project: " + this.name);
+            this.todoLists.forEach(function(list) {
+                list.displayList();
+            });
+        };
+      }
+    }
+  
+    addProject(project) {
+      this.projects.push(project);
+    }
+  
+    removeProject(project) {
+      let index = this.projects.indexOf(project);
+      if (index > -1) {
+        this.projects.splice(index, 1);
+      }
+    }
+  
+    displayProjects() {
+      console.log("Projects:");
+      this.projects.forEach(function(project) {
+        project.displayLists();
+      });
+    }
+  }
 
-    this.addList = function(list) {
-        this.todoLists.push(list);
-    };
+  export {Projects};
+  
 
-    this.removeList = function(list) {
-        let index = this.todoLists.indexOf(list);
-        if (index > -1) {
-            this.todoLists.splice(index, 1);
-        }
-    };
-
-    this.displayLists = function() {
-        console.log("Project: " + this.name);
-        this.todoLists.forEach(function(list) {
-            list.displayList();
-        });
-    };
-}
-
-export {Project};
