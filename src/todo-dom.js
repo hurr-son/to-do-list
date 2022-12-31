f00e55ed06468458bde2aa8f91a22d4f84c28af3
hurr-son <hprewett05@gmail.com>
2022-12-31 12:51:50 -0600

    created a renderproject and renderprojects func

--- src/todo-dom.js
+++ src/todo-dom.js
@@ -1,9 +1,10 @@
-function createToDoList(list) {
+import { Projects } from "./project";
+
+function renderToDoList(list) {
     
-    const content = document.getElementById('content');
     const listCard = document.createElement('div');
     listCard.className = 'list-card'
-    const heading = document.createElement('h2');
+    const heading = document.createElement('h3');
     heading.innerHTML = list.name;
     listCard.appendChild(heading);
 
@@ -36,12 +37,57 @@ function createToDoList(list) {
 
         li.appendChild(checkbox);
         li.appendChild(label);
-
         ul.appendChild(li);
+
     });
-    listCard.appendChild(ul)
-    content.appendChild(heading);
-    content.appendChild(listCard)
+    listCard.appendChild(ul);
+    return listCard;
 }
 
-export {createToDoList};
+
+function renderProject(project) {
+    const content = document.getElementById('content');
+    
+    const projectCard = document.createElement('div');
+    projectCard.className = 'project-card';
+    const heading = document.createElement('h2')
+    heading.innerHTML = project.name;
+    projectCard.appendChild(heading);
+    content.appendChild(projectCard)
+
+    project.todoLists.forEach(list => {
+        projectCard.appendChild(renderToDoList(list));
+        
+    });
+}
+
+
+function renderAllProjects(projects) {
+    const content = document.getElementById('content');
+    
+    const projectsWrapper = document.createElement('div');
+    projectsWrapper.className = 'projects-wrapper';
+
+    projects.forEach(function(project) {
+        const heading = document.createElement('h3');
+        heading.innerHTML = project.name;
+
+        const list = document.createElement('ul');
+
+        project.todoLists.forEach(function(todoList) {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = todoList.name;
+        
+            list.appendChild(listItem);
+        })
+        
+        projectsWrapper.appendChild(heading);
+        projectsWrapper.appendChild(list);
+        content.appendChild(projectsWrapper);
+    })
+
+
+}
+
+
+export {renderToDoList, renderProject, renderAllProjects};
