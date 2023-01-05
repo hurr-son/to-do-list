9cf95097299545c795459581c927cd6caa035eba
hurr-son <hprewett05@gmail.com>
2023-01-05 08:26:27 -0600

    formatted with prettier

--- src/todo.js
+++ src/todo.js
@@ -1,42 +1,38 @@
 function ToDoList(name, description, dueDate, priority, notes) {
-    this.name = name;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.notes = notes;
-    this.completed = false
-    this.items = [];
+  this.name = name;
+  this.description = description;
+  this.dueDate = dueDate;
+  this.priority = priority;
+  this.notes = notes;
+  this.completed = false;
+  this.items = [];
 
-    this.addItem = function(item) {
-        this.items.push(item);
-    };
+  this.addItem = function (item) {
+    this.items.push(item);
+  };
 
-    this.removeItem = function(item) {
-        let index = this.items.indexOf(item);
-        if (index > -1) {
-            this.items.splice(index, 1);
-        }
-        console.log(`Removed: ${item}`);
-    };
-
-    this.setCompleted = function() {
-        this.completed = true;
+  this.removeItem = function (item) {
+    let index = this.items.indexOf(item);
+    if (index > -1) {
+      this.items.splice(index, 1);
     }
+    console.log(`Removed: ${item}`);
+  };
 
+  this.setCompleted = function () {
+    this.completed = true;
+  };
 
-
-    this.displayList = function() {
-        console.log(this.name);
-        console.log("Description: " + this.description);
-        console.log("Due date: " + this.dueDate);
-        console.log("Priority: " + this.priority);
-        console.log("Notes: " + this.notes);
-        for (let i = 0; i < this.items.length; i++) {
-            console.log((i + 1) + ". " + this.items[i]);
-        }
-    };
-
-
+  this.displayList = function () {
+    console.log(this.name);
+    console.log("Description: " + this.description);
+    console.log("Due date: " + this.dueDate);
+    console.log("Priority: " + this.priority);
+    console.log("Notes: " + this.notes);
+    for (let i = 0; i < this.items.length; i++) {
+      console.log(i + 1 + ". " + this.items[i]);
+    }
+  };
 }
 
-export {ToDoList};
+export { ToDoList };
