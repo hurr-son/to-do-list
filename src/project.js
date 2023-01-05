35ef570fa8d2693e07e881a3c6dea9bc9f111e8d
hurr-son <hprewett05@gmail.com>
2023-01-05 08:28:24 -0600

    formatted with prettier

--- src/project.js
+++ src/project.js
@@ -1,82 +1,75 @@
 class Projects {
   constructor() {
     this.projects = [];
-    
-    this.Project = function(name) {
+
+    this.Project = function (name) {
       this.name = name;
       this.todoLists = [];
-      
-      this.addList = function(list) {
+
+      this.addList = function (list) {
         list.id = this.todoLists.length;
         this.todoLists.push(list);
-       
       };
-      
-      this.removeList = function(id) {
-        let index = this.todoLists.findIndex(list => list.id === id);
+
+      this.removeList = function (id) {
+        let index = this.todoLists.findIndex((list) => list.id === id);
         if (index > -1) {
           this.todoLists.splice(index, 1);
         }
       };
-      
-      this.displayLists = function() {
+
+      this.displayLists = function () {
         console.log("Project: " + this.name);
-        this.todoLists.forEach(function(list) {
+        this.todoLists.forEach(function (list) {
           list.displayList();
         });
       };
-    }
-    }
+    };
+  }
 
-    setProject(project) {
-      this.currentProject = new CurrentProject;
-      this.currentProject.setCurrentProject(project);
+  setProject(project) {
+    this.currentProject = new CurrentProject();
+    this.currentProject.setCurrentProject(project);
+  }
 
-    }
+  addProject(project) {
+    project.id = this.projects.length;
+    this.projects.push(project);
+  }
 
-    addProject(project) {
-      project.id = this.projects.length;
-      this.projects.push(project);
-    }
-  
-    removeProject(id) {
-      let index = -1
-      for (let i = 0; i < this.projects.length; i++) {
-        if (this.projects[i].id === id) {
-          index = i
-          break
-        }
+  removeProject(id) {
+    let index = -1;
+    for (let i = 0; i < this.projects.length; i++) {
+      if (this.projects[i].id === id) {
+        index = i;
+        break;
       }
-      if (index > -1) {
-        this.projects.splice(index, 1)
-        
-      }
-      
     }
-    
-    displayProjects() {
-      console.log("Projects:");
-      this.projects.forEach(function(project) {
-        project.displayLists();
-      });
+    if (index > -1) {
+      this.projects.splice(index, 1);
     }
   }
 
-  class CurrentProject {
-    constructor() {
-      this.currentProject = null;
-    }
+  displayProjects() {
+    console.log("Projects:");
+    this.projects.forEach(function (project) {
+      project.displayLists();
+    });
+  }
+}
 
-    setCurrentProject(project) {
-      this.currentProject = project;
-  
-    }
-
-    getCurrentProject() {
-      return this.currentProject;
-    }
+class CurrentProject {
+  constructor() {
+    this.currentProject = null;
   }
 
-  export {Projects, CurrentProject};
-  
+  setCurrentProject(project) {
+    this.currentProject = project;
+  }
 
+  getCurrentProject() {
+    return this.currentProject;
+  }
+}
+
+export { Projects, CurrentProject };
