232d772c7c4b935486f7de39e29fc7e8694aaa1e
hurr-son <hprewett05@gmail.com>
2023-01-05 08:26:58 -0600

    formatted with prettier

--- src/todo-dom.js
+++ src/todo-dom.js
@@ -1,149 +1,135 @@
 import { Projects } from "./project";
 
 function renderToDoList(list) {
-    
-    const listCard = document.createElement('div');
-    listCard.className = 'list-card'
-    listCard.id = list.id
+  const listCard = document.createElement("div");
+  listCard.className = "list-card";
+  listCard.id = list.id;
 
-    
+  const closeButton = document.createElement("span");
+  closeButton.innerHTML = "&times;";
+  closeButton.className = "close-list";
+  closeButton.id = list.id;
 
-    const closeButton = document.createElement('span');
-    closeButton.innerHTML = '&times;';
-    closeButton.className = 'close-list';
-    closeButton.id = list.id
+  listCard.appendChild(closeButton);
+  const heading = document.createElement("h3");
+  heading.className = "list-title";
+  heading.innerHTML = list.name;
+  listCard.appendChild(heading);
 
-    listCard.appendChild(closeButton);
-    const heading = document.createElement('h3');
-    heading.className = 'list-title';
-    heading.innerHTML = list.name;
-    listCard.appendChild(heading);
+  const descSpan = document.createElement("span");
+  descSpan.innerHTML = `Description: ${list.description}`;
+  listCard.appendChild(descSpan);
 
-    const descSpan = document.createElement('span');
-    descSpan.innerHTML = `Description: ${list.description}`;
-    listCard.appendChild(descSpan);
+  const dueDateSpan = document.createElement("span");
+  dueDateSpan.innerHTML = `Due: ${list.dueDate}`;
+  listCard.appendChild(dueDateSpan);
 
-    const dueDateSpan = document.createElement('span');
-    dueDateSpan.innerHTML = `Due: ${list.dueDate}`;
-    listCard.appendChild(dueDateSpan);
+  const prioritySpan = document.createElement("span");
+  prioritySpan.innerHTML = `Priority: ${list.priority}`;
+  listCard.appendChild(prioritySpan);
 
-    const prioritySpan = document.createElement('span');
-    prioritySpan.innerHTML = `Priority: ${list.priority}`;
-    listCard.appendChild(prioritySpan);
+  const ul = document.createElement("ul");
+  ul.className = "list-items";
 
-    const ul = document.createElement("ul");
-    ul.className = 'list-items'
+  list.items.forEach(function (item) {
+    const li = document.createElement("li");
 
-    
-    
-    list.items.forEach(function(item) {
-        
-        const li = document.createElement('li');
-        
-        const itemLi = document.createElement('li');
-        
-        
-        itemLi.innerHTML = item;
-        
-        li.appendChild(itemLi);
-        
-        ul.appendChild(li);
-        
-    });
-    listCard.appendChild(ul);
-    const editButton = document.createElement('button');
-    editButton.type = 'button';
-    editButton.className = 'edit-button';
-    editButton.innerHTML = 'Edit'
-    listCard.appendChild(editButton)
-    return listCard;
+    const itemLi = document.createElement("li");
+
+    itemLi.innerHTML = item;
+
+    li.appendChild(itemLi);
+
+    ul.appendChild(li);
+  });
+  listCard.appendChild(ul);
+  const editButton = document.createElement("button");
+  editButton.type = "button";
+  editButton.className = "edit-button";
+  editButton.innerHTML = "Edit";
+  listCard.appendChild(editButton);
+  return listCard;
 }
-
 
 function renderCreateButton() {
-    const createButton = document.createElement('div');
-    createButton.className = 'create-button';
-    
-    createButton.innerHTML = '+';
-    return createButton;
+  const createButton = document.createElement("div");
+  createButton.className = "create-button";
+
+  createButton.innerHTML = "+";
+  return createButton;
 }
 
-
 function renderProject(project) {
-    const container = document.querySelector('.lists-container');
-    const projectTitle = document.querySelector('.project-title');
-    projectTitle.innerHTML = project.name;
+  const container = document.querySelector(".lists-container");
+  const projectTitle = document.querySelector(".project-title");
+  projectTitle.innerHTML = project.name;
 
-    project.todoLists.forEach(list => {
-        
-        container.appendChild(renderToDoList(list));
-        
-    });
-
-    
+  project.todoLists.forEach((list) => {
+    container.appendChild(renderToDoList(list));
+  });
 }
 
 function renderProjectInput(project) {
-    const projectContainer = document.querySelector('.project-container');
-    
-    const projectsWrapper = document.createElement('div');
-    projectsWrapper.className = 'project-card';
+  const projectContainer = document.querySelector(".project-container");
+
+  const projectsWrapper = document.createElement("div");
+  projectsWrapper.className = "project-card";
+  projectsWrapper.id = project.id;
+
+  const closeButton = document.createElement("span");
+  closeButton.innerHTML = "&times;";
+  closeButton.className = "close-list";
+
+  const heading = document.createElement("h3");
+  heading.innerHTML = project.name;
+
+  const list = document.createElement("ul");
+  projectsWrapper.appendChild(closeButton);
+  projectsWrapper.appendChild(heading);
+  projectsWrapper.appendChild(list);
+
+  project.todoLists.forEach(function (todoList) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = todoList.name;
+
+    list.appendChild(listItem);
+  });
+  projectContainer.appendChild(projectsWrapper);
+}
+
+function renderAllProjects(projects) {
+  const projectContainer = document.querySelector(".project-container");
+
+  projects.forEach(function (project) {
+    const projectsWrapper = document.createElement("div");
+    projectsWrapper.className = "project-card";
     projectsWrapper.id = project.id;
-    
-    const closeButton = document.createElement('span');
-    closeButton.innerHTML = '&times;';
-    closeButton.className = 'close-list';
-    
-    const heading = document.createElement('h3');
+    const closeButton = document.createElement("span");
+    closeButton.innerHTML = "&times;";
+    closeButton.className = "close-list";
+
+    const heading = document.createElement("h3");
     heading.innerHTML = project.name;
-    
-    const list = document.createElement('ul');
+
+    const list = document.createElement("ul");
     projectsWrapper.appendChild(closeButton);
     projectsWrapper.appendChild(heading);
     projectsWrapper.appendChild(list);
-    
-    project.todoLists.forEach(function(todoList) {
-      const listItem = document.createElement('li');
+
+    project.todoLists.forEach(function (todoList) {
+      const listItem = document.createElement("li");
       listItem.innerHTML = todoList.name;
-    
+
       list.appendChild(listItem);
-    })
+    });
     projectContainer.appendChild(projectsWrapper);
-  }
-  
-
-
-function renderAllProjects(projects) {
-    const projectContainer = document.querySelector('.project-container');
-    
-    
-    projects.forEach(function(project) {
-        const projectsWrapper = document.createElement('div');
-        projectsWrapper.className = 'project-card';
-        projectsWrapper.id = project.id
-        const closeButton = document.createElement('span');
-        closeButton.innerHTML = '&times;';
-        closeButton.className = 'close-list';
-       
-        
-        const heading = document.createElement('h3');
-        heading.innerHTML = project.name;
-
-        const list = document.createElement('ul');
-        projectsWrapper.appendChild(closeButton);
-        projectsWrapper.appendChild(heading);
-        projectsWrapper.appendChild(list);
-
-        project.todoLists.forEach(function(todoList) {
-            const listItem = document.createElement('li');
-            listItem.innerHTML = todoList.name;
-        
-            list.appendChild(listItem);
-        })
-        projectContainer.appendChild(projectsWrapper);
-    })
-
-
+  });
 }
 
-export {renderToDoList, renderProject, renderAllProjects, renderCreateButton, renderProjectInput};
+export {
+  renderToDoList,
+  renderProject,
+  renderAllProjects,
+  renderCreateButton,
+  renderProjectInput,
+};
